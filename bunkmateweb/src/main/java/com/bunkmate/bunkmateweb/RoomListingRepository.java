bd00f85c1d60741e162c165aea32dee95c742cd2
rosaluW <113726961+rosaluW@users.noreply.github.com>
2023-04-13 11:30:14 -0700

    Revert "Revert "Added backend code""
    This reverts commit b8c63014a041746a58cd7bfe540c724b198397f0.

---
+++ bunkmateweb/src/main/java/com/bunkmate/bunkmateweb/RoomListingRepository.java
@@ -0,0 +1,9 @@
+package com.bunkmate.bunkmateweb;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomListingRepository extends MongoRepository<RoomListing, ObjectId> {
+}
