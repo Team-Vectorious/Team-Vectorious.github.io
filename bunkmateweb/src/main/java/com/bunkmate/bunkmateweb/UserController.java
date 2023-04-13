1df6827fd08dc94797d71d3e24e964a7bffdc472
rosaluW <113726961+rosaluW@users.noreply.github.com>
2023-04-13 16:01:32 -0700

    Reposting back end

--- bunkmateweb/src/main/java/com/bunkmate/bunkmateweb/UserController.java
+++ bunkmateweb/src/main/java/com/bunkmate/bunkmateweb/UserController.java
@@ -1,11 +1,14 @@
 package com.bunkmate.bunkmateweb;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
--- bunkmateweb/src/main/java/com/bunkmate/bunkmateweb/UserRepository.java
+++ bunkmateweb/src/main/java/com/bunkmate/bunkmateweb/UserRepository.java
@@ -3,6 +3,7 @@ package com.bunkmate.bunkmateweb;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
