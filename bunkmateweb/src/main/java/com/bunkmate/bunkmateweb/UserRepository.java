da1f300acc6e79206c2aa5b2be80795344119108
rosaluW <113726961+rosaluW@users.noreply.github.com>
2023-04-13 11:27:08 -0700

    Added backend code

---
+++ bunkmateweb/src/main/java/com/bunkmate/bunkmateweb/UserRepository.java
@@ -0,0 +1,12 @@
+package com.bunkmate.bunkmateweb;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    Optional<User> findUserByFirstName(String firstName);
+}
